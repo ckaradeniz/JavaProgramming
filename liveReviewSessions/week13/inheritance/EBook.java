f97e16edbc39e6267fbb6c36aff6dd2891b3d560
ckaradeniz <cengizkaradeniz001@gmail.com>
2022-04-10 23:31:58 +0200

    exercises about inheritance

---
+++ liveReviewSessions/week13/inheritance/EBook.java
@@ -0,0 +1,21 @@
+package week13.inheritance;
+
+public class EBook extends Book {
+
+    int size;
+    int pages;
+
+    public EBook() {
+        super();
+    }
+
+    public void readBook(){
+        System.out.println("Reading the book from my Tablet");
+        System.out.println("title = " + title);
+        System.out.println("author = " + author);
+        System.out.println("price = " + price);
+        System.out.println("size = " + size);
+        System.out.println("pages = " + pages);
+    }
+
+}
