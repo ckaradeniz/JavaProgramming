ad375a1608bda1465677ab1a1e21513b6bfc53da
ckaradeniz <cengizkaradeniz001@gmail.com>
2022-04-10 22:59:35 +0200

    exercises about encapsulation person class finished

--- liveReviewSessions/week13/Db.java
+++ liveReviewSessions/week13/Db.java
@@ -48,4 +48,18 @@ Methods:
 - setData - setter method for private data
 - setYint - setter method for private yint
 
+for example
+
+Db db = new Db();
+db.insertData("aaa",123);
+
+System.out.println(db.getData());//aaa
+System.out.println(db.getYint());//123
+
+db.setData("zzz");
+db.setYint(200);
+
+System.out.println(db.getData());//zzz
+System.out.println(db.getYint());//200
+
  */
