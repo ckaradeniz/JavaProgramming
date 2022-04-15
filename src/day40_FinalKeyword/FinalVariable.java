aaef48365e0bec73b49a963c9a456924d3db3939
ckaradeniz <cengizkaradeniz001@gmail.com>
2022-04-15 15:56:46 +0200

    Day40 Protected Access Modifier and Final Keyword

---
+++ src/day40_FinalKeyword/FinalVariable.java
@@ -0,0 +1,52 @@
+package day40_FinalKeyword;
+
+public class FinalVariable {
+
+    final String birthDay;
+    final static String name;
+
+    static {
+        name = "EU8";
+    }
+
+    public FinalVariable(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        final double pi = 3.14;
+
+        //pi = 4.14; final variables can not be reassigned
+        // pi = 5.4;
+
+
+        final String name;
+
+        name = "Java";
+
+        //name = "wooden spoon";
+
+        System.out.println(name);
+
+        System.out.println("---------------------------------------------------------");
+
+        FinalVariable obj = new FinalVariable("May/01");
+
+       // obj.birthDay = "June/01";
+
+        System.out.println(obj.birthDay);
+
+        System.out.println("------------------------------------------------------------");
+
+        //FinalVariable.name = "Python";
+
+
+
+
+
+    }
+
+}
