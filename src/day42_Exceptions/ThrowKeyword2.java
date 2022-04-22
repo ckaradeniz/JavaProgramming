0d19db012418d91782d34ce1d07382f3388342e1
ckaradeniz <cengizkaradeniz001@gmail.com>
2022-04-23 01:36:52 +0200

    day42 Exceptions

---
+++ src/day42_Exceptions/ThrowKeyword2.java
@@ -0,0 +1,18 @@
+package day42_Exceptions;
+
+import java.util.NoSuchElementException;
+
+public class ThrowKeyword2 {
+
+    public static void main(String[] args) {
+
+        throw new NoSuchElementException("No such Element");
+
+        //    throw new InterruptedException();
+
+        //System.out.println("Hello world");
+
+        // throw new Rectangle(5,7);
+
+    }
+}
